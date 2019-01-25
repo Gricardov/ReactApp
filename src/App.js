d9a99bc2b83fffea71902217d169135973c3f3ba
gricardov <gricardov@gmail.com>
2019-01-25 15:28:49 -0500

    first serious commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
 		<div className="container">
 		<NavbarBrand href="/">Mila :(</NavbarBrand>
 		</div>
 		</Navbar>
+		<Menu/>
 		<div>
 	  ¿Por qué me han hecho esto? ¿Por qué todos se han comportado así conmigo? ¿Por qué todos me abandonan cuando los necesito?
 	  </div>
